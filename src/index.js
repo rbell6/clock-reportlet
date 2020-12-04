c8e04d02c73d66b3a36a2b3d04601afa21b8ef99
Richard Bell <rbell@adobe.com>
2020-12-04 16:56:07 -0700

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { render } from "react-dom";
+//import { render } from "react-dom";
 
 function App() {
     const [state, setState] = useState("CLICK ME");
     return <button onClick={() => setState("CLICKED")}>{state}</button>;
 }
 
-render(<App />, document.getElementById("root"));
+// render(<App />, document.getElementById("root"));
+
+export default App;
