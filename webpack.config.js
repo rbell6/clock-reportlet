72c56d0279fa8afebc9c2da2503edde6e94a7f93
Richard Bell <rbell@adobe.com>
2020-12-04 19:26:16 -0700

    ..

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build')
     },
+    optimization: {
+        minimize: false
+    },
     module: {
         rules: [
             {
