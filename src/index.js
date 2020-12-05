cd6d5b3485f4892f0f554c873466c7049852ac72
Richard Bell <rbell@adobe.com>
2020-12-04 21:04:04 -0700

    ...

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 class ClockReportlet extends HTMLDivElement {
     constructor(...args) {
         const self = super(...args);
-
+        console.log('ClockReportlet instantiated')
         setInterval(() => {
             const date = new Date();
             self.textContent = date.toDateString() + ' ' + date.toTimeString();
